c3cf7c614fd5c7e32cb64e9fb7993f2ce5bdcbd0
Jordan Daniel 27 <jordanmoreano.ppy@gmail.com>
2024-04-18 21:28:14 -0500

    agregar comprobante

--- src/js/prueba.js
+++ src/js/prueba.js
@@ -1,17 +1,19 @@
-var input = document.querySelector(".input-box");
-input.textContent = "Arequipa"; // Dato que llega de la bd
+document.addEventListener("DOMContentLoaded", function () {
+  var input = document.querySelector(".input-box");
+  input.textContent = "Arequipa"; // Dato que llega de la bd
 
-var indexDepa = null;
-console.log(input.textContent.trim());
+  var indexDepa = null;
+  console.log(input.textContent.trim());
 
-const radio = document.querySelectorAll(".radio");
+  const radio = document.querySelectorAll(".radio");
 
-radio.forEach((item) => {
-  if (item.classList.contains("Arequipa")) {
-    item.checked = true;
-    indexDepa = item.getAttribute("depa-atributo");
-    return;
-  }
+  radio.forEach((item) => {
+    if (item.classList.contains("Arequipa")) {
+      item.checked = true;
+      indexDepa = item.getAttribute("depa-atributo");
+      return;
+    }
+  });
+
+  console.log(indexDepa);
 });
-
-console.log(indexDepa);
